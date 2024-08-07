a2be5732229a962d88c2f5bda1591b552109bc03
KC <kiranchauhan68@gmail.com>
2024-08-07 23:00:45 +0530

    Chapter 02

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import postgres from "postgres";
+
+const sql = postgres({
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: "hono",
+});
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello!');
+app.get("/", async (c) => {
+  const result = await sql`SELECT 1 + 1`;
+  console.log(result);
+  return c.text("Hello!");
 });
 
 serve(app, (info) => {
